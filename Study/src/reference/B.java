372560df350e88916ab9acebdd6a9f9cf4e0dd03
Tu <tu7a1k23@gmail.com>
2018-08-29 01:15:48 +0700

    reference

---
+++ Study/src/reference/B.java
@@ -0,0 +1,10 @@
+/*
+	Hi Brian T!
+*/
+package reference;
+
+public class B {
+	A xc = new A();
+	String b = xc.sstr;
+	String a = A.str;
+}
--- Study/src/study/A.java
+++
@@ -1,9 +0,0 @@
-/*
-	Hi Brian T!
-*/
-package study;
-
-public class A {
-	public static String str ="sà"; 
-	public  String sstr ="sà";
-}
